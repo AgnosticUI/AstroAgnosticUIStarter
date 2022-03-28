3c8d9bfe2292b0785afb62de167de063be315977
Rob Levin <roblevintennis@gmail.com>
2022-03-27 20:45:19 -0500

    Refactor astro configuration to use integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
-  },
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
+
+export default defineConfig({
+	integrations: [vue(), react(), svelte()],
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: [
-    "@astrojs/renderer-react",
-    "@astrojs/renderer-svelte",
-    "@astrojs/renderer-vue"
-  ],
-};
+    hostname: 'localhost',
+    port: 3012,
+  }
+});
